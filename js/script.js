0df34e6e7fcdda704d392cec03e0bc35cb8bfc35
syed asim <aasimshah8110@gmail.com>
2024-01-12 10:59:00 +0500

    frist

---
+++ js/script.js
@@ -0,0 +1,10 @@
+  $(document).ready(function() {
+    $('#openMenu').on('click' , () =>{
+      $("#menu_sideBar").show()
+    })
+    $('#closeMenu').on('click' , () =>{
+      $("#menu_sideBar").hide()
+    })
+
+    console.log("first")
+  });
